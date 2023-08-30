eee840210a7b833d0ae90a927e8195756057dc0e
Ivan test <curtees1@gmail.com>
2023-08-30 15:54:40 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 25853;
+        int mili = ticketPrice / 20;
+
+        System.out.println("Мили за покупку билета:" + mili);
+
+
+
+    }
+}
